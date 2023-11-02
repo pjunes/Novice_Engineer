25b17b109931c243395c5e0ba53b787e4db44460
pjunes <qwt1597@me.com>
2023-11-02 17:36:37 +0900

    ENH: new project network_socket

---
+++ Network_Socket/CMakeFiles/Network_Socket_test.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Network_Socket_test.dir/main.cpp.o"
+  "bin/Network_Socket_test"
+  "bin/Network_Socket_test.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Network_Socket_test.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
